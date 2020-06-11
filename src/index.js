793349ec2d420d2ac08d4e5dce9b7fad039a8332
Pat Faiola <pat.faiola@fahmpartners.com>
2020-06-10 23:19:31 -0400

    Shop Number

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import pkg from "../package.json";
 import schemas from './schemas';
 import resolvers from './resolvers';
+import { ShopHours } from './simpleSchemas';
 
 /**
  * @summary Import and call this function to add this plugin to your API.
@@ -15,6 +16,13 @@ export default async function register(app) {
     graphQL: {
       resolvers,
       schemas
+    }, 
+    shop: {
+      shopNo: "shopNo",
+      shopHours: ["shopHours"]
+    },
+    simpleSchemas: {
+      ShopHours
     }
   });
 }
---
+++ src/simpleSchemas.js
@@ -0,0 +1,41 @@
+import SimpleSchema from "simpl-schema";
+import {
+    Shop
+} from '/imports/collections/schemas';
+
+/**
+ * @name ShopHours
+ * @memberof Schemas
+ * @type {SimpleSchema}
+ * @property {String} day required
+ * @property {String} start required
+ * @property {String} end required
+ */
+export const ShopHours = new SimpleSchema({
+    day: {
+      type: String
+    },
+    start: {
+      type: String
+    }, 
+    end: {
+      type: String
+    }
+  });
+
+  
+const schemaExtension = {
+    "shopNo": {
+        type: String,
+        optional: true
+    },
+    "shopHours": {
+        type: Array,
+        optional: true
+    },
+    "shopHours.$": {
+        type: ShopHours
+    }
+}
+
+Shop.extend(schemaExtension);
